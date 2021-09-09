29baa3b849639e837fcdf5668f6bef328d791c99
Zac Allen <zac.allen@outlook.com>
2021-09-09 10:15:16 +0200

    added transmit function to demonstrate bits lost in transmit

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import random
+
 class KeyStream:
     def __init__(self, key=1):
         self.next = key
@@ -13,15 +15,28 @@ class KeyStream:
 def encrypt(key, message):
     return bytes([message[i] ^ key.get_key_byte() for i in range(len(message))])
 
+# function to demonstrate bits lost in transmit
+def transmit(cipher, likely):
+    b = []
+    for c in cipher:
+        if random.randrange(0, likely) == 0:
+            c = c ^ 2**random.randrange(0, 8)
+        b.append(c)
+    return bytes(b)
+
 
 key = KeyStream(10)
 # for i in range(10):
 #     print(key.get_key_byte())
 
 # encrypt message
-message = "Hello, World!".encode()
+message = "Hello, World! This is a test for demonstration purposes.".encode()
 cipher = encrypt(key, message)
+print(message)
 print(cipher)
+
+# calling transmit to demonstrate bits lost in transmit
+cipher = transmit(cipher, 5)
 
 # decrypt message using KeyStream
 key = KeyStream(10)
